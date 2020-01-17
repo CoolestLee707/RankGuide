e0e8a37f15196ad712f906c2f61707f4c99d64fe
李传敏 <lichuanmin@daojia.com.cn>
2020-01-17 16:15:44 +0800

    搜索、本地存储
    SharedPreferences

--- lib/second_demos/search_home.dart
+++ lib/second_demos/search_home.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'model/rank_search_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
 
 class SearchHome extends StatefulWidget {
-
   int sceneItemNumber = 3;
 
   SearchHome({
@@ -15,7 +16,6 @@ class SearchHome extends StatefulWidget {
 }
 
 class _SearchHomeState extends State<SearchHome> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,13 +23,14 @@ class _SearchHomeState extends State<SearchHome> {
         title: Text('搜索'),
         elevation: 0.0,
       ),
-      body: MainBody(selectNumber: widget.sceneItemNumber,),
+      body: MainBody(
+        selectNumber: widget.sceneItemNumber,
+      ),
     );
   }
 }
 
 class MainBody extends StatefulWidget {
-  
   int selectNumber;
 
   MainBody({
@@ -40,36 +41,28 @@ class MainBody extends StatefulWidget {
 }
 
 class _MainBodyState extends State<MainBody> {
-
   searchkeysModel keysModel;
   String _selectButton = '';
   List<String> _buttonTextList = ['商品榜', '美食榜', '医药榜', '汽车榜'];
   List<String> allSearchTextList = [];
-  List<String> HistorySearchTextList = [
-    '商品榜',
-    '美食榜',
-    '医药榜',
-    '汽车榜',
-    '商品榜',
-    '美食榜',
-    '医药榜',
-    '汽车榜'
-  ];
+  List<String> HistorySearchTextList = [];
 
   requestData(int scene) async {
-
     Response response;
     Dio dio = Dio();
-    response = await dio.post(searchKeysUrl().returnUrl(),
-        data: {"scene": scene});
+    response =
+        await dio.post(searchKeysUrl().returnUrl(), data: {"scene": scene});
 
     keysModel = searchkeysModel.fromJson(response.data['data']);
+
+    // 读取历史搜索
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
     setState(() {
       allSearchTextList = keysModel.keywords;
+      HistorySearchTextList = prefs.getStringList('search_history') ?? [];
     });
-
   }
-
 
   selectButtonStyle(String item) {
     if (item == _selectButton) {
@@ -109,93 +102,129 @@ class _MainBodyState extends State<MainBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey[100],
-      child: Padding(
-        padding: EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Flexible(
-                  flex: 5,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20.0),
-                    child: Container(
-                      height: 40.0,
-                      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-                      color: Colors.white,
-                      child: TextField(
-                        decoration: InputDecoration(
+    String _searchKeys;
+
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        color: Colors.grey[100],
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 5,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Container(
+                        height: 40.0,
+                        padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                        color: Colors.white,
+                        child: TextField(
+                          // 设置键盘类型右下角按键
+                          textInputAction: TextInputAction.search,
+                          decoration: InputDecoration(
                             border: InputBorder.none,
                             hintText: '输入搜索关键字',
-                            icon: Icon(Icons.search,color: Colors.black87,),
+                            icon: Icon(
+                              Icons.search,
+                              color: Colors.black87,
                             ),
-                        onChanged: (value) {
-                          print("回调了 $value");
-                        },
+                          ),
+                          onChanged: (value) {
+                            _searchKeys = value;
+                          },
 
-                        ///键盘上按了done
-                        onEditingComplete: () {},
+                          ///键盘上按了done
+                          onSubmitted: (value) async {
+                            SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+                            this.HistorySearchTextList = prefs.getStringList('search_history') ?? [];
+                              setState(() {
+                                this.HistorySearchTextList.add(value);
+                              });
+                            prefs.setStringList('search_history', this.HistorySearchTextList);
+                            FocusScope.of(context).requestFocus(FocusNode());
+
+                            Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('search ${value}。。。'),
+                                action: SnackBarAction(
+                                  label: 'OK',
+                                  onPressed: (){
+
+                                  },
+                                ),
+                              )
+                            );
+                            
+                          },
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Flexible(
-                  flex: 1,
-                  child: FlatButton(
-                    child: Text(
-                      '取消',
-                      style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black),
+                  Flexible(
+                    flex: 1,
+                    child: FlatButton(
+                      child: Text(
+                        '取消',
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
                     ),
-                    onPressed: () {},
                   ),
-                ),
-              ],
-            ),
-            Container(
-              color: Colors.grey[100],
-              height: 60.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: _buttonTextList.map((item) {
-                  return FlatButton(
-                    child: Text(
-                      item,
-                      style: selectButtonStyle(item),
-                    ),
-                    onPressed: () {
-                      print(item);
-                      setState(() {
-                        _selectButton = item;
-                        int selectItem = 3;
-                        if (item == _buttonTextList[0]) {
-                          selectItem = 3;
-                        }else if(item == _buttonTextList[1]) {
-                          selectItem = 4;
-                        }else if(item == _buttonTextList[2]) {
-                          selectItem = 7;
-                        }else {
-                          selectItem = 8;
-                        }
-                        requestData(selectItem);
-                      });
-                    },
-                  );
-                }).toList(),
+                ],
               ),
-            ),
-            SearchTips(
-              allSearchTextList: this.allSearchTextList,
-              historySearchTextList: this.HistorySearchTextList,
-            ),
-          ],
+              Container(
+                color: Colors.grey[100],
+                height: 60.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: _buttonTextList.map((item) {
+                    return FlatButton(
+                      child: Text(
+                        item,
+                        style: selectButtonStyle(item),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _selectButton = item;
+                          int selectItem = 3;
+                          if (item == _buttonTextList[0]) {
+                            selectItem = 3;
+                          } else if (item == _buttonTextList[1]) {
+                            selectItem = 4;
+                          } else if (item == _buttonTextList[2]) {
+                            selectItem = 7;
+                          } else {
+                            selectItem = 8;
+                          }
+                          requestData(selectItem);
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              SearchTips(
+                allSearchTextList: this.allSearchTextList,
+                historySearchTextList: this.HistorySearchTextList,
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -271,12 +300,17 @@ class AllPeopleSearch extends StatelessWidget {
   }
 }
 
-class HistorySearch extends StatelessWidget {
+class HistorySearch extends StatefulWidget {
   List<String> historySearchTextList;
   HistorySearch({
     this.historySearchTextList,
   });
 
+  @override
+  _HistorySearchState createState() => _HistorySearchState();
+}
+
+class _HistorySearchState extends State<HistorySearch> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -298,18 +332,30 @@ class HistorySearch extends StatelessWidget {
                   color: Colors.black,
                 ),
               ),
-              // Icon(Icons.delete,color: Colors.red,),
               ClipRRect(
                 borderRadius: BorderRadius.circular(5.0),
                 child: FlatButton.icon(
-                  icon: Icon(Icons.delete,color: Colors.red,),
-                  label: Text('清空',style: TextStyle(color: Colors.red,fontSize: 15.0,fontWeight: FontWeight.w400),),
-                  onPressed: (){
-                    print('清空');
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                  label: Text(
+                    '清空',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  onPressed: () async {
+                    SharedPreferences prefs = await SharedPreferences.getInstance();
+                    prefs.remove('search_history');
+
+                    setState(() {
+                      widget.historySearchTextList = [];
+                    });
                   },
                 ),
               )
-
             ],
           ),
           SizedBox(
@@ -318,7 +364,7 @@ class HistorySearch extends StatelessWidget {
           Wrap(
             spacing: 15.0,
             runSpacing: 4.0,
-            children: this.historySearchTextList.map((item) {
+            children: widget.historySearchTextList.map((item) {
               return ActionChip(
                 label: Text(item),
                 onPressed: () {
